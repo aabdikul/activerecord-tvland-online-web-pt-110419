fec33c4711378c39ef99f2856bfe2aa85a3a71fe
Aida Abdikulova <abdikulova@gmail.com>
2020-02-11 02:32:55 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,12 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    Actor.each do |object|
+      name = object.first_name
+    end
+    name
+  end
+
 end
